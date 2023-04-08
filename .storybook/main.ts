324f2bee2af7d0f84fda422601cde7dd448ef507
yutakam80 <yutakam80@gmail.com>
2023-04-08 12:46:28 +0900

    chore: v7 にアップデート

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,26 @@
 import type { StorybookViteConfig } from '@storybook/builder-vite';
 import { mergeConfig } from 'vite';
 import path from 'path';
-
 const config: StorybookViteConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
   },
+  core: {},
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
   async viteFinal(config) {
     return mergeConfig(config, {
       resolve: {
         alias: {
-          src: path.resolve(__dirname, '../src'),
-        },
-      },
+          src: path.resolve(__dirname, '../src')
+        }
+      }
     });
-  },
+  }
 };
-
 export default config;
+export const framework = '@storybook/react';
