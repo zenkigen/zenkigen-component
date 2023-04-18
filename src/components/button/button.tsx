9092e5334d218afcbdc2146d5e075525b9918ff2
yutakam80 <yutakam80@gmail.com>
2023-04-18 16:00:49 +0900

    fix: styled-components を剥がす

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,59 +1,14 @@
 import { ReactNode } from 'react';
 
-import styled, { css } from 'styled-components';
-
-import { useTheme } from '../../hooks';
-import { CreateTheme } from '../../theme';
-
 type Props = {
-  disabled?: boolean;
   isAnchor?: boolean;
   children?: ReactNode;
 };
 
-export function Button({ disabled, isAnchor, children }: Props) {
-  const theme = useTheme();
-
+export function Button({ isAnchor, children }: Props) {
   if (isAnchor) {
-    return <StyledAnchor theme={theme}>{children}</StyledAnchor>;
+    return <a>{children}</a>;
   } else {
-    return (
-      <StyledButton theme={theme} disabled={disabled}>
-        {children}
-      </StyledButton>
-    );
+    return <button>{children}</button>;
   }
 }
-
-const baseStyles = (theme: CreateTheme) => css`
-  ${theme.LabelLabel414pxRegular};
-  color: ${theme.TokensTextTextOnColor};
-  border-radius: 4px;
-  padding: ${theme.space(3)} ${theme.space(5)};
-  background-color: ${theme.TokensInteractiveInteractive01};
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${theme.TokensHoverHover01};
-  }
-
-  &:active {
-    background-color: ${theme.TokensActiveActive01};
-  }
-
-  &:disabled {
-    background-color: ${theme.TokensDisabledDisabled01};
-    pointer-events: none;
-    cursor: not-allowed;
-  }
-`;
-
-const StyledButton = styled.button<{ theme: CreateTheme }>`
-  ${({ theme }) => baseStyles(theme)}
-  border: none;
-`;
-
-const StyledAnchor = styled.a<{ theme: CreateTheme }>`
-  ${({ theme }) => baseStyles(theme)}
-  text-decoration: none;
-`;
