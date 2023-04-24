fcaef18b24de310013aa92bf78378a99e286afc3
yutakam80 <yutakam80@gmail.com>
2023-04-24 17:45:55 +0900

    feat: add heading

---
+++ packages/components/src/heading/heading.stories.tsx
@@ -0,0 +1,45 @@
+import { Icon } from '../icon';
+
+import { Heading } from '.';
+
+export default {
+  component: Heading,
+};
+
+export function Base() {
+  return (
+    <div style={{ display: 'grid', gap: '30px' }}>
+      <Heading
+        level={1}
+        before={<Icon name="chart-bar" size="huge" />}
+        after={<Icon name="information" size="large" />}
+      >
+        タイトル
+      </Heading>
+      <Heading
+        level={2}
+        before={<Icon name="chart-bar" size="huge" />}
+        after={<Icon name="information" size="large" />}
+      >
+        タイトル
+      </Heading>
+      <Heading level={3} before={<Icon name="chart-bar" size="huge" />} after={<Icon name="information" />}>
+        タイトル
+      </Heading>
+      <Heading
+        level={4}
+        before={<Icon name="chart-bar" size="small" />}
+        after={<Icon name="information" size="small" />}
+      >
+        タイトル
+      </Heading>
+      <Heading
+        level={5}
+        before={<Icon name="chart-bar" size="small" />}
+        after={<Icon name="information" size="small" />}
+      >
+        タイトル
+      </Heading>
+    </div>
+  );
+}
