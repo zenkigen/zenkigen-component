bd3ff03566fd584c2b5331c5bfc4cc00e2e82b72
keroken <kenichi.shinagawa@gmail.com>
2023-04-27 00:06:28 +0900

    update Icon - add className prop, add interactive color

--- packages/components/src/icon/icon.tsx
+++ packages/components/src/icon/icon.tsx
@@ -10,12 +10,13 @@ type Props = {
   name: IconName;
   size?: Size;
   color?: Color;
+  className?: string;
 };
 
 export const Icon = ({ size = 'medium', ...props }: Props) => {
   return (
     <span
-      className={classNames('inline-block', {
+      className={classNames('inline-block', props.className, {
         [iconColors.icon01]: props.color === 'icon01',
         [iconColors.icon01Dark]: props.color === 'icon01Dark',
         [iconColors.icon02]: props.color === 'icon02',
@@ -23,6 +24,7 @@ export const Icon = ({ size = 'medium', ...props }: Props) => {
         [iconColors.icon03]: props.color === 'icon03',
         [iconColors.icon03Dark]: props.color === 'icon03Dark',
         [iconColors.iconOnColor]: props.color === 'iconOnColor',
+        [iconColors.interactive01]: props.color === 'interactive01',
         'w-3 h-3': size === 'x-small',
         'w-4 h-4': size === 'small',
         'w-6 h-6': size === 'medium',
