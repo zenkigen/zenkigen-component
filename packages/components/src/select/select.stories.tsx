fe51f547f9f43b3c7ff493d0e5cc3d8e8470191a
keroken <kenichi.shinagawa@gmail.com>
2023-04-27 20:25:31 +0900

    update Select story to show controls

--- packages/components/src/select/select.stories.tsx
+++ packages/components/src/select/select.stories.tsx
@@ -1,41 +1,40 @@
-import { SelectOption } from './select';
+import type { Meta, StoryObj } from '@storybook/react';
 
-import { Select } from '.';
+import { Select, SelectOption } from './select';
 
-export default {
+const meta: Meta<typeof Select> = {
   component: Select,
 };
-
-export function Base() {
-  return (
-    <div>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '80px', marginBottom: '200px' }}>
-        <Select size="small" variant="outline" options={optionsList1} />
-        <Select
-          size="small-medium"
-          variant="outline"
-          options={optionsList1}
-          placeholder={{
-            id: 'placeholder',
-            value: '選択してください',
-            icon: 'add',
-          }}
-        />
-        <Select size="medium" variant="outline" options={optionsList1} />
-        <Select size="large" variant="outline" options={optionsList1} />
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '80px' }}>
-        <Select size="small" variant="text" options={optionsList1} />
-        <Select size="small-medium" variant="text" options={optionsList1} />
-        <Select size="medium" variant="text" options={optionsList1} />
-        <Select size="large" variant="text" options={optionsList1} />
-      </div>
-    </div>
-  );
-}
+export default meta;
+type Story = StoryObj<typeof Select>;
 
 const optionsList1 = [
-  { id: '1', value: '発話比率', icon: 'add' },
-  { id: '2', value: '体の向き', icon: 'add' },
-  { id: '3', value: '笑顔度', icon: 'add' },
+  { id: '1', value: '選択肢A', icon: 'add' },
+  { id: '2', value: '選択肢B', icon: 'add' },
+  { id: '3', value: '選択肢C', icon: 'add' },
 ] as SelectOption[];
+
+const placeholder1: SelectOption = {
+  id: 'placeholder',
+  value: '選択してください',
+  icon: 'add',
+};
+
+export const Base: Story = {
+  render: ({ ...args }) => (
+    <>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '80px', marginBottom: '200px' }}>
+        <Select {...args} size="small" variant="outline" options={optionsList1} placeholder={placeholder1} />
+        <Select {...args} size="small-medium" variant="outline" options={optionsList1} placeholder={placeholder1} />
+        <Select {...args} size="medium" variant="outline" options={optionsList1} placeholder={placeholder1} />
+        <Select {...args} size="large" variant="outline" options={optionsList1} placeholder={placeholder1} />
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '80px' }}>
+        <Select {...args} size="small" variant="text" options={optionsList1} placeholder={placeholder1} />
+        <Select {...args} size="small-medium" variant="text" options={optionsList1} placeholder={placeholder1} />
+        <Select {...args} size="medium" variant="text" options={optionsList1} placeholder={placeholder1} />
+        <Select {...args} size="large" variant="text" options={optionsList1} placeholder={placeholder1} />
+      </div>
+    </>
+  ),
+};
