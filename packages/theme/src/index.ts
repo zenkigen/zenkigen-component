09da8d828093555ecbd349e236eb625857bd6e20
keroken <kenichi.shinagawa@gmail.com>
2023-05-08 19:04:08 +0900

    add z-index to theme

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,3 +1,4 @@
 export * from './colors';
 export * from './form';
 export * from './typography';
+export * from './z-index';
---
+++ packages/theme/src/z-index.ts
@@ -0,0 +1,14 @@
+export const zIndex = {
+  hide: -1,
+  auto: 'auto',
+  base: 0,
+  Badge: 10,
+  Header: 100,
+  dropdown: 300,
+  Popover: 500,
+  Overlay: 1000,
+  Modal: 1100,
+  Preloader: 1200,
+  Toast: 1300,
+  Tooltip: 1400,
+};
