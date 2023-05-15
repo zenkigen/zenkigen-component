b1a0a079c96512827bda8c6b6d1e834d507fca39
keroken <kenichi.shinagawa@gmail.com>
2023-05-15 14:16:16 +0900

    add focusWithin to theme/src/form.ts

--- packages/theme/src/form.ts
+++ packages/theme/src/form.ts
@@ -4,3 +4,9 @@ export const focusVisible = {
   inset:
     'focus-visible:outline focus-visible:outline-2 focus-visible:outline-focus-focus focus-visible:outline-offset-[-2px]',
 };
+
+export const focusWithin = {
+  normal: 'focus-within:outline focus-within:outline-2 focus-within:outline-focus-focus focus-within:outline-offset-1',
+  inset:
+    'focus-within:outline focus-within:outline-2 focus-within:outline-focus-focus focus-within:outline-offset-[-2px]',
+};
