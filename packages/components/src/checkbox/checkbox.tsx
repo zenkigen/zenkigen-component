bbaa74fcd8692ff89ad8aec83ea6d13b2ba13caa
yuki.ito <yuki.ito@zenkigen.jp>
2023-05-16 17:36:56 +0900

    icon divide file

--- packages/components/src/checkbox/checkbox.tsx
+++ packages/components/src/checkbox/checkbox.tsx
@@ -3,6 +3,9 @@ import { ChangeEvent, useCallback, useState } from 'react';
 import { focusVisible, typography } from '@zenkigen-component/theme';
 import clsx from 'clsx';
 
+import { CheckedIcon } from './checked-icon';
+import { MinusIcon } from './minus-icon';
+
 type Props = {
   name?: string;
   value?: string;
@@ -96,8 +99,6 @@ export function Checkbox({
     'block',
     'm-auto',
     'rounded-sm',
-    'transition-transform',
-    'duration-150',
     isDisabled && isChecked
       ? 'bg-disabled-disabled01'
       : color === 'gray'
@@ -125,24 +126,6 @@ export function Checkbox({
     isDisabled ? 'pointer-events-none cursor-not-allowed text-disabled-disabled01' : 'cursor-pointer text-text-text01',
   );
 
-  const svgClasses = clsx('z-10', 'absolute', 'h-5', 'w-5', 'fill-icon-iconOnColor', 'rounded-sm', 'hover:rounded-sm');
-
-  const CheckedIcon = (
-    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" className={svgClasses}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16.2887 5.38099C16.0069 5.09924 15.5518 5.09924 15.2701 5.38099L7.55461 13.0893L4.72993 10.2646C4.44818 9.98283 3.99305 9.98283 3.71131 10.2646C3.42956 10.5463 3.42956 11.0014 3.71131 11.2832L7.04891 14.6208C7.1934 14.7653 7.374 14.8303 7.56183 14.8303C7.74966 14.8303 7.93027 14.7581 8.07475 14.6208L16.2887 6.40683C16.5704 6.12508 16.5704 5.66273 16.2887 5.38099Z"
-      />
-    </svg>
-  );
-
-  const MinusIcon = (
-    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" className={svgClasses}>
-      <path d="M4.94723 10.5028H9.49726H10.5028H15.0528C15.3293 10.5028 15.5556 10.2766 15.5556 10C15.5556 9.72352 15.3293 9.49725 15.0528 9.49725H10.5028H9.49726H4.94723C4.67071 9.49725 4.44446 9.72352 4.44446 10C4.44446 10.2766 4.67071 10.5028 4.94723 10.5028Z" />
-    </svg>
-  );
-
   return (
     <div className={wrapperClasses}>
       <div className={baseClasses}>
@@ -161,8 +144,10 @@ export function Checkbox({
         <div color={color} className={boxClasses}>
           <div color={color} className={indicatorClasses}>
             <span className={afterClasses}>
-              {isChecked && !isIndeterminate && CheckedIcon}
-              {isIndeterminate && MinusIcon}
+              <>
+                {isChecked && !isIndeterminate && CheckedIcon}
+                {isIndeterminate && MinusIcon}
+              </>
             </span>
           </div>
         </div>
