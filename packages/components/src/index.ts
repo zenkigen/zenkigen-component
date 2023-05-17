0e8ff9e03be7cbbfcfd463d0ec5cac3bae973bcd
keroken <kenichi.shinagawa@gmail.com>
2023-05-17 19:48:12 +0900

    add select-sort, tooltip and alphabetically sort checkbox

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,7 @@
 export * from './avatar';
 export * from './breadcrumb';
 export * from './button';
+export * from './checkbox';
 export * from './heading';
 export * from './icon';
 export * from './icon-button';
@@ -8,5 +9,6 @@ export * from './loading';
 export * from './notification-inline';
 export * from './search';
 export * from './select';
+export * from './select-sort';
 export * from './tab';
-export * from './checkbox';
+export * from './tooltip';
