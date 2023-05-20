4e4f777f856eb8b7a87a24338a53c162ecf3c501
keroken <kenichi.shinagawa@gmail.com>
2023-05-20 22:01:49 +0900

    separate childrenButtonClasses

--- packages/components/src/dropdown/dropdown.tsx
+++ packages/components/src/dropdown/dropdown.tsx
@@ -75,10 +75,7 @@ export function Dropdown({
 
   const wrapperClasses = clsx(
     'relative',
-    'flex',
-    'shrink-0',
-    'gap-1',
-    'items-center',
+    'flex shrink-0 items-center gap-1',
     'rounded',
     'bg-background-uiBackground01',
     !children && {
@@ -89,23 +86,29 @@ export function Dropdown({
     isDisabled && 'cursor-not-allowed',
   );
 
+  const childrenButtonClasses = clsx(
+    'flex items-center',
+    'rounded',
+    'p-1',
+    'hover:bg-hover-hover02',
+    'active:bg-active-active02',
+    focusVisible.normal,
+    isDisabled && 'pointer-events-none',
+  );
+
   const buttonClasses = clsx(
-    'flex',
-    'items-center',
-    'w-full',
-    'h-full',
+    'flex items-center',
+    'h-full w-full',
     'rounded',
     buttonColors[variant].base,
     buttonColors[variant].hover,
     buttonColors[variant].active,
     buttonColors[variant].disabled,
     focusVisible.normal,
-    children
-      ? 'p-1'
-      : {
-          'px-2': size === 'x-small' || size === 'small',
-          'px-4': size === 'medium' || size === 'large',
-        },
+    {
+      'px-2': size === 'x-small' || size === 'small',
+      'px-4': size === 'medium' || size === 'large',
+    },
     isDisabled && 'pointer-events-none',
   );
 
@@ -120,23 +123,23 @@ export function Dropdown({
 
   return (
     <div ref={targetRef} className={wrapperClasses}>
-      <button type="button" className={buttonClasses} onClick={handleToggle} disabled={isDisabled}>
-        {children ? (
-          childrenWithProps
-        ) : (
-          <>
-            {icon && (
-              <span className="mr-1 flex">
-                <Icon name={icon} size={size === 'large' ? 'medium' : 'small'} />
-              </span>
-            )}
-            <span className={labelClasses}>{label}</span>
-            <div className="ml-auto flex items-center">
-              <Icon name={isVisible ? 'angle-small-up' : 'angle-small-down'} size="small" />
-            </div>
-          </>
-        )}
-      </button>
+      {children ? (
+        <button type="button" className={childrenButtonClasses} onClick={handleToggle} disabled={isDisabled}>
+          {childrenWithProps}
+        </button>
+      ) : (
+        <button type="button" className={buttonClasses} onClick={handleToggle} disabled={isDisabled}>
+          {icon && (
+            <span className="mr-1 flex">
+              <Icon name={icon} size={size === 'large' ? 'medium' : 'small'} />
+            </span>
+          )}
+          <span className={labelClasses}>{label}</span>
+          <div className="ml-auto flex items-center">
+            <Icon name={isVisible ? 'angle-small-up' : 'angle-small-down'} size="small" />
+          </div>
+        </button>
+      )}
       {!isDisabled && isVisible && (
         <DropdownMenu
           variant={variant}
