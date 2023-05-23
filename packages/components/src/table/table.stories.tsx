6c002af2e661af67dd916fdddce54dff66b50249
keroken <kenichi.shinagawa@gmail.com>
2023-05-23 14:40:04 +0900

    update story: add SelectSort

--- packages/components/src/table/table.stories.tsx
+++ packages/components/src/table/table.stories.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from 'react';
+
 import { action } from '@storybook/addon-actions';
 import type { Meta, StoryObj } from '@storybook/react';
 import { typography } from '@zenkigen-component/theme';
@@ -7,6 +9,8 @@ import { Avatar } from '../avatar';
 import { Dropdown } from '../dropdown';
 import { DropdownItemType } from '../dropdown/type';
 import { Icon } from '../icon';
+import { SelectSort } from '../select-sort';
+import { SortOrder } from '../select-sort/type';
 
 import { TableContainer, TableRowContainer, TableHeading, TableCell } from '.';
 
@@ -42,6 +46,25 @@ const items: DropdownItemType[] = [
 ];
 
 const TableStoryBasic = () => {
+  const [sortKey, setSortKey] = useState<'name' | 'email' | null>(null);
+  const [sortOrderName, setSortOrderName] = useState<SortOrder>(null);
+  const [sortOrderEmail, setSortOrderEmail] = useState<SortOrder>(null);
+
+  const handleSortName = useCallback((direction: SortOrder) => {
+    setSortOrderName(direction);
+    setSortOrderEmail(null);
+    setSortKey('name');
+  }, []);
+  const handleSortEmail = useCallback((direction: SortOrder) => {
+    setSortOrderEmail(direction);
+    setSortOrderName(null);
+    setSortKey('email');
+  }, []);
+  const handleClickDeselect = useCallback(() => {
+    setSortKey(null);
+    setSortOrderName(null);
+    setSortOrderEmail(null);
+  }, []);
   const headingClasses = clsx(typography.label.label2regular, 'text-text-text02');
   const cellClasses = clsx(typography.label.label1regular, 'text-text-text01', 'flex items-center', 'gap-2');
 
@@ -50,10 +73,30 @@ const TableStoryBasic = () => {
       <TableContainer rows="40px repeat(3, 48px)" columns="200px 400px 1fr 80px">
         <TableRowContainer>
           <TableHeading>
-            <div className={headingClasses}>氏名</div>
+            <div className={headingClasses}>
+              <SelectSort
+                size="small"
+                variant="text"
+                label="氏名"
+                sortOrder={sortOrderName}
+                isSortKey={sortKey === 'name'}
+                onChange={handleSortName}
+                onClickDeselect={handleClickDeselect}
+              />
+            </div>
           </TableHeading>
           <TableHeading>
-            <div className={headingClasses}>メールアドレス</div>
+            <div className={headingClasses}>
+              <SelectSort
+                size="small"
+                variant="text"
+                label="メールアドレス"
+                sortOrder={sortOrderEmail}
+                isSortKey={sortKey === 'email'}
+                onChange={handleSortEmail}
+                onClickDeselect={handleClickDeselect}
+              />
+            </div>
           </TableHeading>
           <TableHeading>
             <div className={headingClasses}>権限</div>
