01ed8216bd750235efbbb81213f28d435eefcdb9
keroken <kenichi.shinagawa@gmail.com>
2023-05-23 14:23:35 +0900

    update story: add Avatar and Dropdown as examples

--- packages/components/src/table/table.stories.tsx
+++ packages/components/src/table/table.stories.tsx
@@ -1,4 +1,12 @@
+import { action } from '@storybook/addon-actions';
 import type { Meta, StoryObj } from '@storybook/react';
+import { typography } from '@zenkigen-component/theme';
+import clsx from 'clsx';
+
+import { Avatar } from '../avatar';
+import { Dropdown } from '../dropdown';
+import { DropdownItemType } from '../dropdown/type';
+import { Icon } from '../icon';
 
 import { TableContainer, TableRowContainer, TableHeading, TableCell } from '.';
 
@@ -9,33 +17,105 @@ const meta: Meta<typeof TableContainer> = {
 export default meta;
 type Story = StoryObj<typeof TableContainer>;
 
+const items: DropdownItemType[] = [
+  {
+    id: '1',
+    icon: 'edit' as const,
+    label: '操作項目1',
+    color: 'gray' as const,
+    onClick: action('選択項目1'),
+  },
+  {
+    id: '2',
+    icon: 'edit' as const,
+    label: '操作項目2',
+    color: 'gray' as const,
+    onClick: action('選択項目2'),
+  },
+  {
+    id: '3',
+    icon: 'edit' as const,
+    label: '操作項目3',
+    color: 'red' as const,
+    onClick: action('選択項目3'),
+  },
+];
+
 const TableStoryBasic = () => {
+  const headingClasses = clsx(typography.label.label2regular, 'text-text-text02');
+  const cellClasses = clsx(typography.label.label1regular, 'text-text-text01', 'flex items-center', 'gap-2');
+
   return (
-    <div className="flex items-center">
-      <TableContainer rows="repeat(4, 1fr)" columns="repeat(4, 1fr)">
+    <div className="flex items-center p-10">
+      <TableContainer rows="40px repeat(3, 48px)" columns="200px 400px 1fr 80px">
         <TableRowContainer>
-          <TableHeading>項目1</TableHeading>
-          <TableHeading>項目2</TableHeading>
-          <TableHeading>項目3</TableHeading>
-          <TableHeading>項目4</TableHeading>
+          <TableHeading>
+            <div className={headingClasses}>氏名</div>
+          </TableHeading>
+          <TableHeading>
+            <div className={headingClasses}>メールアドレス</div>
+          </TableHeading>
+          <TableHeading>
+            <div className={headingClasses}>権限</div>
+          </TableHeading>
+          <TableHeading />
         </TableRowContainer>
         <TableRowContainer>
-          <TableCell>アイテム1</TableCell>
-          <TableCell>アイテム2</TableCell>
-          <TableCell>アイテム3</TableCell>
-          <TableCell>アイテム4</TableCell>
+          <TableCell>
+            <div className={cellClasses}>
+              <Avatar size="x-small" userId={1} lastName="全機現" firstName="太郎" />
+              全機現太郎
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className={cellClasses}>taro.zenkigen@zk-creative.jp</div>
+          </TableCell>
+          <TableCell>
+            <div className={cellClasses}>管理者</div>
+          </TableCell>
+          <TableCell align="right">
+            <Dropdown size="x-small" items={items} horizontalAlign="right">
+              <Icon name="more" size="small" color="icon01" />
+            </Dropdown>
+          </TableCell>
         </TableRowContainer>
         <TableRowContainer>
-          <TableCell>アイテム1</TableCell>
-          <TableCell>アイテム2</TableCell>
-          <TableCell>アイテム3</TableCell>
-          <TableCell>アイテム4</TableCell>
+          <TableCell>
+            <div className={cellClasses}>
+              <Avatar size="x-small" userId={1} lastName="全機現" firstName="太郎" />
+              全機現太郎
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className={cellClasses}>taro.zenkigen@zk-creative.jp</div>
+          </TableCell>
+          <TableCell>
+            <div className={cellClasses}>管理者</div>
+          </TableCell>
+          <TableCell align="right">
+            <Dropdown size="x-small" items={items} horizontalAlign="right">
+              <Icon name="more" size="small" color="icon01" />
+            </Dropdown>
+          </TableCell>
         </TableRowContainer>
         <TableRowContainer>
-          <TableCell>アイテム1</TableCell>
-          <TableCell>アイテム2</TableCell>
-          <TableCell>アイテム3</TableCell>
-          <TableCell>アイテム4</TableCell>
+          <TableCell>
+            <div className={cellClasses}>
+              <Avatar size="x-small" userId={1} lastName="全機現" firstName="太郎" />
+              全機現太郎
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className={cellClasses}>taro.zenkigen@zk-creative.jp</div>
+          </TableCell>
+          <TableCell>
+            <div className={cellClasses}>管理者</div>
+          </TableCell>
+          <TableCell align="right">
+            <Dropdown size="x-small" items={items} horizontalAlign="right">
+              <Icon name="more" size="small" color="icon01" />
+            </Dropdown>
+          </TableCell>
         </TableRowContainer>
       </TableContainer>
     </div>
