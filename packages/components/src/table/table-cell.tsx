f803176c180d496c325ef937218dfab9698a09be
keroken <kenichi.shinagawa@gmail.com>
2023-05-25 11:07:56 +0900

    add className prop to TableCell

--- packages/components/src/table/table-cell.tsx
+++ packages/components/src/table/table-cell.tsx
@@ -5,13 +5,14 @@ import clsx from 'clsx';
 type Props = {
   children?: ReactNode;
   align?: 'left' | 'center' | 'right';
+  className?: string;
 };
 
-export function TableCell({ children, align = 'left' }: Props) {
+export function TableCell({ children, align = 'left', className }: Props) {
   const cellClasses = clsx('flex items-center', 'border-b-[1px] border-border-uiBorder01', {
     'justify-start': align === 'left',
     'justify-center': align === 'center',
     'justify-end': align === 'right',
   });
-  return <div className={cellClasses}>{children}</div>;
+  return <div className={clsx(cellClasses, className)}>{children}</div>;
 }
