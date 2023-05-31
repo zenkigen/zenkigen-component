c0e863ad1127940a65af2d422ebd3c3214e8205a
keroken <kenichi.shinagawa@gmail.com>
2023-05-31 15:09:56 +0900

    update story: display size and isEditable variants

--- packages/components/src/evaluation-star/evaluation-star.stories.tsx
+++ packages/components/src/evaluation-star/evaluation-star.stories.tsx
@@ -7,5 +7,12 @@ export default {
 };
 
 export const Base = {
-  render: ({ ...args }) => <EvaluationStar value={3} size="large" onChangeRating={action('評価変更')} {...args} />,
+  render: () => (
+    <div className="grid gap-y-8">
+      <EvaluationStar value={3} size="medium" />
+      <EvaluationStar value={3} size="medium" isEditable onChangeRating={action('評価変更')} />
+      <EvaluationStar value={3} size="large" />
+      <EvaluationStar value={3} size="large" isEditable onChangeRating={action('評価変更')} />
+    </div>
+  ),
 };
