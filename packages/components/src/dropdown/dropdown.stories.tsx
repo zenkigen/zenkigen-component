a721ca516f3354a63e6842e86a445395a36cc38e
keroken <kenichi.shinagawa@gmail.com>
2023-06-09 10:50:50 +0900

    create story with custom menu component

--- packages/components/src/dropdown/dropdown.stories.tsx
+++ packages/components/src/dropdown/dropdown.stories.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 import { action } from '@storybook/addon-actions';
 import type { Meta, StoryObj } from '@storybook/react';
+import { typography } from '@zenkigen-component/theme';
+import clsx from 'clsx';
 
 import { Avatar } from '../avatar';
+import { Button } from '../button';
 import { Icon } from '../icon';
+import { Toggle } from '../toggle';
 
 import { Dropdown } from './dropdown';
 import { DropdownItemType } from './type';
@@ -88,4 +94,77 @@ const DropdownBasic = () => {
 
 export const Basic: Story = {
   render: () => <DropdownBasic />,
+};
+
+const DropdownWithCustomMenu = () => {
+  const [isOn1, setIsOn1] = useState(false);
+  const [isOn2, setIsOn2] = useState(false);
+  const [isOn3, setIsOn3] = useState(false);
+
+  const handleClickReset = () => {
+    setIsOn1(false);
+    setIsOn2(false);
+    setIsOn3(false);
+  };
+
+  const items = [
+    {
+      id: '1',
+      label: '発話比率',
+      color: 'fill-user-turquoise',
+      isChecked: isOn1,
+      onChange: () => setIsOn1((prev) => !prev),
+    },
+    {
+      id: '2',
+      label: '体の向き',
+      color: 'fill-user-blue',
+      isChecked: isOn2,
+      onChange: () => setIsOn2((prev) => !prev),
+    },
+    {
+      id: '3',
+      label: '会話テンポ',
+      color: 'fill-user-pink',
+      isChecked: isOn3,
+      onChange: () => setIsOn3((prev) => !prev),
+    },
+  ];
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', margin: '60px 100px' }}>
+      <Dropdown
+        size="medium"
+        label="フィルター"
+        icon="filter"
+        horizontalAlign="right"
+        menuComponent={
+          <ul className="flex w-[208px] flex-col gap-y-2.5 px-4 py-3">
+            {items.map((item) => (
+              <li key={item.id} className={clsx('flex w-full items-center', item.color)}>
+                <Icon name="graph-line" size="small" />
+                <span className={clsx('ml-2 flex-1 text-text-text01', typography.label.label2regular)}>
+                  {item.label}
+                </span>
+                <Toggle id="1" size="small" isChecked={item.isChecked} onChange={item.onChange} />
+              </li>
+            ))}
+            <div className="flex justify-end">
+              <Button
+                variant="outline"
+                size="medium"
+                onClick={handleClickReset}
+                isDisabled={!isOn1 && !isOn2 && !isOn3}
+              >
+                初期値に戻す
+              </Button>
+            </div>
+          </ul>
+        }
+      />
+    </div>
+  );
+};
+
+export const WithCustomMenu: Story = {
+  render: () => <DropdownWithCustomMenu />,
 };
