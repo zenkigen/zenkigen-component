cbc323f0cda7b8942daef844ecb34710fd347640
yutakam80 <yutakam80@gmail.com>
2023-06-09 23:53:42 +0900

    feat: add defaultValue

--- packages/components/src/search/search.tsx
+++ packages/components/src/search/search.tsx
@@ -8,52 +8,55 @@ import { Icon } from '../icon';
 type Props = {
   placeholder?: string;
   width?: string;
+  defaultValue?: string;
   onSubmit: (keyword: string) => void;
   onClickSearchModal?: () => void;
 };
 
-export const Search = forwardRef<HTMLDivElement, Props>(({ width = '100%', ...props }: Props, ref) => {
-  const [text, setText] = useState('');
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    props.onSubmit(text);
-  };
+export const Search = forwardRef<HTMLDivElement, Props>(
+  ({ width = '100%', defaultValue = '', ...props }: Props, ref) => {
+    const [text, setText] = useState(defaultValue);
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      props.onSubmit(text);
+    };
 
-  const wrapperClasses = clsx('relative');
-  const searchAreaClasses = clsx(
-    'flex',
-    'items-center',
-    'h-8',
-    'px-3',
-    'rounded-full',
-    'border',
-    'border-border-uiBorder02',
-    'focus-within:border-active-activeInput',
-  );
-  const searchInputClasses = clsx(
-    typography.label.label2regular,
-    'placeholder:text-text-textPlaceholder',
-    'ml-3',
-    'outline-0',
-    'flex-1',
-    'h-full',
-  );
+    const wrapperClasses = clsx('relative');
+    const searchAreaClasses = clsx(
+      'flex',
+      'items-center',
+      'h-8',
+      'px-3',
+      'rounded-full',
+      'border',
+      'border-border-uiBorder02',
+      'focus-within:border-active-activeInput',
+    );
+    const searchInputClasses = clsx(
+      typography.label.label2regular,
+      'placeholder:text-text-textPlaceholder',
+      'ml-3',
+      'outline-0',
+      'flex-1',
+      'h-full',
+    );
 
-  return (
-    <div className={wrapperClasses} ref={ref}>
-      <form onSubmit={handleSubmit}>
-        <div className={searchAreaClasses} style={{ width }}>
-          <Icon name="search" color="icon01" size="small" />
-          <input
-            type="text"
-            size={1}
-            className={searchInputClasses}
-            placeholder={props.placeholder}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </div>
-      </form>
-    </div>
-  );
-});
+    return (
+      <div className={wrapperClasses} ref={ref}>
+        <form onSubmit={handleSubmit}>
+          <div className={searchAreaClasses} style={{ width }}>
+            <Icon name="search" color="icon01" size="small" />
+            <input
+              type="text"
+              size={1}
+              className={searchInputClasses}
+              placeholder={props.placeholder}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  },
+);
 Search.displayName = 'Search';
