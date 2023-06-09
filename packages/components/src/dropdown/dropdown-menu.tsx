c4bb2c3ea18728bd16302e37278391a540a74a77
keroken <kenichi.shinagawa@gmail.com>
2023-06-09 10:49:40 +0900

    adjust menu style

--- packages/components/src/dropdown/dropdown-menu.tsx
+++ packages/components/src/dropdown/dropdown-menu.tsx
@@ -1,51 +1,59 @@
+import { ReactElement } from 'react';
+
 import clsx from 'clsx';
 
 import { DropdownItem } from './dropdown-item';
 import { DropdownItemType, DropdownHorizontalAlign, DropdownVerticalPosition } from './type';
 
-type Props = {
-  variant: 'text' | 'outline';
-  items: DropdownItemType[];
-  verticalPosition?: DropdownVerticalPosition;
-  horizontalAlign?: DropdownHorizontalAlign;
-  targetDimensions: { width: number; height: number };
-  onClickItem: (onClickAction: () => void) => void;
-};
+type Props =
+  | {
+      variant: 'text' | 'outline';
+      verticalPosition?: DropdownVerticalPosition;
+      horizontalAlign?: DropdownHorizontalAlign;
+      targetDimensions: { width: number; height: number };
+      onClickItem: (onClickAction: () => void) => void;
+    } & ({ items: DropdownItemType[]; menuComponent?: never } | { items?: never; menuComponent: ReactElement });
 
 export function DropdownMenu({
   variant,
   items,
+  menuComponent,
   verticalPosition,
   horizontalAlign,
   targetDimensions,
   onClickItem,
 }: Props) {
-  const listClasses = clsx(
+  const wrapperClasses = clsx(
     'z-dropdown',
     'absolute',
-    'w-max',
-    'py-2',
-    'overflow-y-auto',
     'bg-background-uiBackground01',
     'rounded',
     'shadow-componentShadow',
+    'overflow-y-auto',
     horizontalAlign === 'left' ? 'left-0' : horizontalAlign === 'right' ? 'right-0' : 'left-auto',
     {
       'border-solid border border-border-uiBorder01': variant === 'outline',
     },
   );
+  const menuClasses = clsx('w-max', 'py-2');
 
   return (
-    <ul
-      className={listClasses}
+    <div
+      className={wrapperClasses}
       style={{
         top: verticalPosition === 'bottom' ? `${targetDimensions.height + 4}px` : 'unset',
         bottom: verticalPosition === 'top' ? `${targetDimensions.height + 4}px` : 'unset',
       }}
     >
-      {items.map((item) => (
-        <DropdownItem key={item.id} item={item} onClickItem={() => onClickItem(item.onClick)} />
-      ))}
-    </ul>
+      {items ? (
+        <ul className={menuClasses}>
+          {items.map((item) => (
+            <DropdownItem key={item.id} item={item} onClickItem={() => onClickItem(item.onClick)} />
+          ))}
+        </ul>
+      ) : (
+        menuComponent
+      )}
+    </div>
   );
 }
