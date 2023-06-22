29cfe109d9e2615d8e6673934712d5cdb0cc6a00
keroken <kenichi.shinagawa@gmail.com>
2023-06-22 19:34:42 +0900

    add checkbox and sub button option to modal-bottom-tab

--- packages/components/src/modal/modal-button-tab.tsx
+++ packages/components/src/modal/modal-button-tab.tsx
@@ -3,17 +3,36 @@ import { useContext } from 'react';
 import clsx from 'clsx';
 
 import { Button } from '../button';
+import { Checkbox } from '../checkbox';
 
 import { ModalContext } from './modal-context';
 
-type Props = {
-  primaryButtonLabel: string;
-  secondaryButtonLabel: string;
-  onClickPrimaryButton: () => void;
-  onClickSecondaryButton: () => void;
-  isDanger?: boolean;
-  isNoBorder?: boolean;
-};
+type Props =
+  | {
+      primaryButtonLabel: string;
+      secondaryButtonLabel: string;
+      onClickPrimaryButton: () => void;
+      onClickSecondaryButton: () => void;
+      isDanger?: boolean;
+      isNoBorder?: boolean;
+    } & (
+      | {
+          isWithCheckbox?: boolean;
+          checkboxLabel?: string;
+          isChecked?: boolean;
+          onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+          subButtonLabel?: never;
+          onClickSubButton?: never;
+        }
+      | {
+          isWithCheckbox?: never;
+          checkboxLabel?: never;
+          isChecked?: never;
+          onChange?: never;
+          subButtonLabel?: string;
+          onClickSubButton?: () => void;
+        }
+    );
 
 export function ModalButtonTab({
   primaryButtonLabel,
@@ -22,6 +41,12 @@ export function ModalButtonTab({
   onClickSecondaryButton,
   isDanger,
   isNoBorder,
+  isWithCheckbox,
+  checkboxLabel,
+  isChecked,
+  onChange,
+  subButtonLabel,
+  onClickSubButton,
 }: Props) {
   const { size, setIsOpen } = useContext(ModalContext);
   const handleClickPrimaryButton = () => {
@@ -32,42 +57,54 @@ export function ModalButtonTab({
     onClickSecondaryButton();
     setIsOpen(false);
   };
+  const handleClickSubButton = () => {
+    onClickSubButton?.();
+  };
 
-  const footerClasses = clsx(
-    'flex',
-    'shrink-0',
-    'justify-end',
-    'items-center',
-    'w-full',
-    'h-[72px]',
-    'rounded-b-lg',
-    'px-6',
-    {
-      'gap-x-2': size === 'small',
-      'gap-x-4': size === 'medium' || size === 'large' || size === 'x-large',
-      'border-t-[1px] border-border-uiBorder01': !isNoBorder,
-    },
-  );
+  const wrapperClasses = clsx('flex', 'shrink-0', 'items-center', 'w-full', 'rounded-b-lg', 'py-4', 'px-6', {
+    'justify-between': isWithCheckbox || (subButtonLabel && size !== 'small'),
+    'justify-end': !isWithCheckbox || !subButtonLabel,
+    'justify-center': subButtonLabel && size === 'small',
+    'border-t-[1px] border-border-uiBorder01': !isNoBorder,
+    'gap-y-4': size === 'small',
+    'flex-wrap': size === 'small' && isWithCheckbox,
+    'flex-wrap-reverse': size === 'small' && subButtonLabel,
+  });
+  const buttonContainerClasses = clsx('flex', 'items-center', {
+    'gap-x-2': size === 'small',
+    'gap-x-4': size === 'medium' || size === 'large' || size === 'x-large',
+  });
   return (
-    <div className={footerClasses}>
-      <Button
-        key="1"
-        variant="outline"
-        size="large"
-        width={size === 'small' ? 132 : 'auto'}
-        onClick={handleClickSecondaryButton}
-      >
-        {secondaryButtonLabel}
-      </Button>
-      <Button
-        key="2"
-        variant={isDanger ? 'fillDanger' : 'fill'}
-        size="large"
-        width={size === 'small' ? 132 : 'auto'}
-        onClick={handleClickPrimaryButton}
-      >
-        {primaryButtonLabel}
-      </Button>
+    <div className={wrapperClasses}>
+      {isWithCheckbox ? (
+        <div>
+          <Checkbox label={checkboxLabel} isChecked={isChecked} onChange={onChange} />
+        </div>
+      ) : subButtonLabel && onClickSubButton ? (
+        <Button key="0" variant="text" size="large" onClick={handleClickSubButton}>
+          {subButtonLabel}
+        </Button>
+      ) : null}
+      <div className={buttonContainerClasses}>
+        <Button
+          key="1"
+          variant="outline"
+          size="large"
+          width={size === 'small' ? 132 : 'auto'}
+          onClick={handleClickSecondaryButton}
+        >
+          {secondaryButtonLabel}
+        </Button>
+        <Button
+          key="2"
+          variant={isDanger ? 'fillDanger' : 'fill'}
+          size="large"
+          width={size === 'small' ? 132 : 'auto'}
+          onClick={handleClickPrimaryButton}
+        >
+          {primaryButtonLabel}
+        </Button>
+      </div>
     </div>
   );
 }
