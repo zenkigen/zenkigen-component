b3ca1efa70f05b99d5bce3834ce9ba935af5653c
keroken <kenichi.shinagawa@gmail.com>
2023-06-22 15:10:54 +0900

    update modal-header: add children and isNoCloseButton prop

--- packages/components/src/modal/modal-header.tsx
+++ packages/components/src/modal/modal-header.tsx
@@ -1,5 +1,6 @@
-import { useContext } from 'react';
+import { ReactNode, useContext } from 'react';
 
+import { typography } from '@zenkigen-component/theme';
 import clsx from 'clsx';
 
 import { IconButton } from '../icon-button';
@@ -7,18 +8,32 @@ import { IconButton } from '../icon-button';
 import { ModalContext } from './modal-context';
 
 type Props = {
+  children: ReactNode;
   isNoBorder?: boolean;
+  isNoCloseButton?: boolean;
 };
 
-export function ModalHeader({ isNoBorder }: Props) {
-  const { onClose } = useContext(ModalContext);
-  const headerClasses = clsx('flex', 'items-center', 'justify-between', 'w-full', 'h-14', 'rounded-t-lg', 'px-6', {
-    'border border-b-[1px] border-border-uiBorder01': !isNoBorder,
-  });
+export function ModalHeader({ children, isNoBorder, isNoCloseButton }: Props) {
+  const { setIsOpen } = useContext(ModalContext);
+  const headerClasses = clsx(
+    'flex',
+    'items-center',
+    'justify-between',
+    'w-full',
+    'rounded-t-lg',
+    'px-6',
+    'text-text-text01',
+    typography.heading.h5,
+    {
+      'border-b-[1px] border-border-uiBorder01': !isNoBorder,
+      'h-14': !isNoCloseButton,
+      'h-12': isNoCloseButton,
+    },
+  );
   return (
     <div className={headerClasses}>
-      Title
-      <IconButton icon="close" size="small" variant="text" onClick={onClose} />
+      {children}
+      {!isNoCloseButton && <IconButton icon="close" size="small" variant="text" onClick={() => setIsOpen(false)} />}
     </div>
   );
 }
