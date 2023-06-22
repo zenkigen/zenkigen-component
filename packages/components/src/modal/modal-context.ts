f4fadf372039df5fdd037cf2cf0ced80ebf3ceda
keroken <kenichi.shinagawa@gmail.com>
2023-06-22 15:07:45 +0900

    update modal-context

--- packages/components/src/modal/modal-context.ts
+++ packages/components/src/modal/modal-context.ts
@@ -1,13 +1,9 @@
 import { createContext } from 'react';
 
-import { WidthVariant } from './type';
-
 type ModalReturnType = {
-  onClose: () => void;
-  widthVariant?: WidthVariant;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const ModalContext = createContext<ModalReturnType>({
-  onClose: () => false,
-  widthVariant: 'normal',
+  setIsOpen: () => false,
 });
