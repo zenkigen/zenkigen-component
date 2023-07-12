44ed4b5986307448a5b6a241b2e17db48a8193d5
keroken <kenichi.shinagawa@gmail.com>
2023-07-12 18:43:56 +0900

    delete button background: Dropdown, Select, SelectSort

--- packages/components/src/select/select.tsx
+++ packages/components/src/select/select.tsx
@@ -43,21 +43,12 @@ export function Select({
 
   const handleClickToggle = () => setIsOptionListOpen((prev) => !prev);
 
-  const wrapperClasses = clsx(
-    'relative',
-    'flex',
-    'shrink-0',
-    'gap-1',
-    'items-center',
-    'rounded',
-    'bg-background-uiBackground01',
-    {
-      'h-6': size === 'x-small' || size === 'small',
-      'h-8': size === 'medium',
-      'h-10': size === 'large',
-      'cursor-not-allowed': isDisabled,
-    },
-  );
+  const wrapperClasses = clsx('relative', 'flex', 'shrink-0', 'gap-1', 'items-center', 'rounded', {
+    'h-6': size === 'x-small' || size === 'small',
+    'h-8': size === 'medium',
+    'h-10': size === 'large',
+    'cursor-not-allowed': isDisabled,
+  });
 
   const buttonClasses = clsx(
     'flex',
