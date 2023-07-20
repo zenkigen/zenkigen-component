d86ac621bc728c9c258e1e14d3ce5e8adfa0018f
yutakam80 <yutakam80@gmail.com>
2023-07-20 16:57:05 +0900

    feat: fontFamily  を extend する

--- packages/config/src/tailwind-config.ts
+++ packages/config/src/tailwind-config.ts
@@ -3,13 +3,13 @@ import { tokens } from './tokens/tokens';
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: `'Noto Sans JP', ${tokens.fontFamilies.helvetica}, sans-serif`,
+      },
       colors: {
         user: tokens.user,
         ...tokens.tokens,
         ...tokens.colors,
-      },
-      fontFamily: {
-        body: `'Noto Sans JP', ${tokens.fontFamilies.helvetica}, sans-serif`,
       },
       fontSize: tokens.fontSize,
       lineHeight: tokens.lineHeights,
