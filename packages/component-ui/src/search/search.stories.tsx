2888aab5aea6ce112baa72bb6ad0a654ae726268
kenichi iida <kenichi.iida@quq.jp>
2023-08-08 13:32:54 +0900

    fix: icon size, add clear button.

--- packages/component-ui/src/search/search.stories.tsx
+++ packages/component-ui/src/search/search.stories.tsx
@@ -1,7 +1,26 @@
+import { FormEvent, ChangeEvent } from 'react';
+
+import { action } from '@storybook/addon-actions';
+
 import { Search } from '.';
 
 export default {
   component: Search,
 };
 
-export const Base = { args: { placeholder: '検索' } };
+export function Base() {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    action('onSubmit')(e);
+  };
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    action('onChange')(e);
+  };
+
+  return (
+    <div>
+      <Search placeholder="検索" value="" onSubmit={handleSubmit} onChange={handleOnChange}></Search>
+    </div>
+  );
+}
--- packages/component-ui/src/search/search.tsx
+++ packages/component-ui/src/search/search.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, forwardRef } from 'react';
+import { ChangeEvent, FormEvent, useState, useEffect, useRef, forwardRef } from 'react';
 
 import { typography } from '@zenkigen-inc/component-theme';
 
@@ -13,24 +13,47 @@ type Props = {
   onClickSearchModal?: () => void;
 };
 
-export const Search = forwardRef<HTMLDivElement, Props>(({ width = '100%', value = '', ...props }: Props, ref) => (
-  <div className="relative" ref={ref}>
-    <form onSubmit={props.onSubmit}>
-      <div
-        className="flex h-8 items-center rounded-full border border-border-uiBorder02 px-3 focus-within:border-active-activeInput"
-        style={{ width }}
-      >
-        <Icon name="search" color="icon01" size="small" />
-        <input
-          type="text"
-          size={1}
-          value={value}
-          className={`${typography.label.label2regular} ml-3 h-full flex-1 outline-0 placeholder:text-text-textPlaceholder`}
-          placeholder={props.placeholder}
-          onChange={props.onChange}
-        />
+export const Search = forwardRef<HTMLDivElement, Props>(
+  ({ width = '100%', value: propValue, ...props }: Props, ref) => {
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const handleClickClear = () => {
+      setValue('');
+    };
+
+    useEffect(() => {
+      propValue && setValue(propValue);
+    }, [propValue]);
+
+    return (
+      <div className="relative" ref={ref}>
+        <form onSubmit={props.onSubmit}>
+          <div
+            className="flex h-8 items-center rounded-full border border-border-uiBorder02 px-3 focus-within:border-active-activeInput"
+            style={{ width }}
+          >
+            <Icon name="search" color="icon01" size="medium" />
+            <input
+              type="text"
+              size={1}
+              value={value}
+              className={`${typography.label.label2regular} ml-3 h-full flex-1 outline-0 placeholder:text-text-textPlaceholder`}
+              placeholder={props.placeholder}
+              onChange={(e) => {
+                setValue(e.target.value);
+                props.onChange && props.onChange(e);
+              }}
+              ref={inputRef}
+            />
+            {value && value.length !== 0 && (
+              <button type="button" onClick={() => handleClickClear()} className="flex h-4">
+                <Icon name="close" color="icon01" size="small" />
+              </button>
+            )}
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-));
+    );
+  },
+);
 Search.displayName = 'Search';
