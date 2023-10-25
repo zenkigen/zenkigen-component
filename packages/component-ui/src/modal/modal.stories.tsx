0402064663881fcfa1f3269ddc94166eade63e78
yutakam80 <yutakam80@gmail.com>
2023-10-25 12:29:17 +0900

    fix: modal の width を考慮してスタイルを変更しない

--- packages/component-ui/src/modal/modal.stories.tsx
+++ packages/component-ui/src/modal/modal.stories.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
-import clsx from 'clsx';
 
 import { Button } from '../button';
 import { Checkbox } from '../checkbox';
@@ -12,33 +11,19 @@ import { Modal } from '.';
 
 const meta: Meta<typeof Modal> = {
   component: Modal,
-  argTypes: {
-    width: {
-      type: 'number',
-    },
-  },
 };
 
 type Story = StoryObj<typeof Modal>;
-
-const widthSmall = 320;
-const widthMedium = 480;
-const widthLimit = 420;
 
 export default meta;
 
 export const Base: Story = {
   args: {
-    width: widthMedium,
+    width: 480,
   },
   render: function MyFunc({ ...args }) {
     const [, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
+
     return (
       <div>
         <button type="button" onClick={() => setIsOpen(true)}>
@@ -50,21 +35,11 @@ export const Base: Story = {
             <div className="flex h-[200px] w-full items-center justify-center">Content</div>
           </Modal.Body>
           <Modal.Footer>
-            <div className={footerClasses}>
-              <Button
-                variant="outline"
-                size="large"
-                onClick={action('キャンセル')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+            <div className="flex w-full flex-wrap items-center justify-end gap-4">
+              <Button variant="outline" size="large" onClick={action('キャンセル')}>
                 キャンセル
               </Button>
-              <Button
-                variant="fill"
-                size="large"
-                onClick={action('保存する')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+              <Button variant="fill" size="large" onClick={action('保存する')}>
                 保存する
               </Button>
             </div>
@@ -77,23 +52,12 @@ export const Base: Story = {
 
 export const WithCheckbox: Story = {
   args: {
-    width: widthMedium,
+    width: 480,
   },
   render: function MyFunc({ ...args }) {
     const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'justify-between', {
-      'flex-col gap-4': args.width < widthLimit,
-    });
-    const footerLeftBoxClasses = clsx('flex', 'items-center', 'flex-wrap', {
-      'w-full': args.width < widthLimit,
-    });
-    const footerButtonClasses = clsx('flex', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
     const [isChecked, setIsChecked] = useState(false);
+
     return (
       <div>
         <button type="button" onClick={() => setIsOpen(true)}>
@@ -105,8 +69,8 @@ export const WithCheckbox: Story = {
             <div className="flex h-[200px] w-full items-center justify-center">Content</div>
           </Modal.Body>
           <Modal.Footer>
-            <div className={footerClasses}>
-              <div className={footerLeftBoxClasses}>
+            <div className="flex w-full justify-between gap-4">
+              <div className="flex flex-wrap items-center">
                 <Checkbox
                   id="modal-checkbox"
                   label="ラベル"
@@ -114,21 +78,11 @@ export const WithCheckbox: Story = {
                   onChange={() => setIsChecked((prev) => !prev)}
                 />
               </div>
-              <div className={footerButtonClasses}>
-                <Button
-                  variant="outline"
-                  size="large"
-                  onClick={action('キャンセル')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
+              <div className="flex flex-wrap items-center justify-end gap-4">
+                <Button variant="outline" size="large" onClick={action('キャンセル')}>
                   キャンセル
                 </Button>
-                <Button
-                  variant="fill"
-                  size="large"
-                  onClick={action('保存する')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
+                <Button variant="fill" size="large" onClick={action('保存する')}>
                   保存する
                 </Button>
               </div>
@@ -142,20 +96,11 @@ export const WithCheckbox: Story = {
 
 export const WithSubButton: Story = {
   args: {
-    width: widthMedium,
+    width: 480,
   },
   render: function MyFunc({ ...args }) {
     const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'justify-between', {
-      'flex-col-reverse gap-4': args.width < widthLimit,
-    });
-    const footerLeftBoxClasses = 'flex items-center flex-wrap';
-    const footerButtonClasses = clsx('flex', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
+
     return (
       <div>
         <button type="button" onClick={() => setIsOpen(true)}>
@@ -167,199 +112,17 @@ export const WithSubButton: Story = {
             <div className="flex h-[200px] w-full items-center justify-center">Content</div>
           </Modal.Body>
           <Modal.Footer>
-            <div className={footerClasses}>
-              <div className={footerLeftBoxClasses}>
+            <div className="flex w-full items-center justify-between">
+              <div className="flex flex-wrap items-center">
                 <Button variant="text" size="large" onClick={action('ボタンラベル')}>
                   ボタンラベル
                 </Button>
               </div>
-              <div className={footerButtonClasses}>
-                <Button
-                  variant="outline"
-                  size="large"
-                  onClick={action('キャンセル')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
+              <div className="flex flex-wrap items-center justify-end gap-4">
+                <Button variant="outline" size="large" onClick={action('キャンセル')}>
                   キャンセル
                 </Button>
-                <Button
-                  variant="fill"
-                  size="large"
-                  onClick={action('保存する')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
-                  保存する
-                </Button>
-              </div>
-            </div>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  },
-};
-
-export const SizeSmall: Story = {
-  args: {
-    width: widthSmall,
-  },
-  render: function MyFunc({ ...args }) {
-    const [, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
-    return (
-      <div>
-        <button type="button" onClick={() => setIsOpen(true)}>
-          open
-        </button>
-        <Modal isOpen setIsOpen={setIsOpen} width={args.width}>
-          <Modal.Header>タイトル</Modal.Header>
-          <Modal.Body>
-            <div className="flex h-[200px] w-full items-center justify-center">Content</div>
-          </Modal.Body>
-          <Modal.Footer>
-            <div className={footerClasses}>
-              <Button
-                variant="outline"
-                size="large"
-                onClick={action('キャンセル')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
-                キャンセル
-              </Button>
-              <Button
-                variant="fill"
-                size="large"
-                onClick={action('保存する')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
-                保存する
-              </Button>
-            </div>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  },
-};
-
-export const SizeSmallWithCheckbox: Story = {
-  args: {
-    width: widthSmall,
-  },
-  render: function MyFunc({ ...args }) {
-    const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'justify-between', {
-      'flex-col gap-4': args.width < widthLimit,
-    });
-    const footerLeftBoxClasses = clsx('flex', 'items-center', 'flex-wrap', {
-      'w-full': args.width < widthLimit,
-    });
-    const footerButtonClasses = clsx('flex', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
-    const [isChecked, setIsChecked] = useState(false);
-    return (
-      <div>
-        <button type="button" onClick={() => setIsOpen(true)}>
-          open
-        </button>
-        <Modal isOpen setIsOpen={setIsOpen} width={args.width}>
-          <Modal.Header>タイトル</Modal.Header>
-          <Modal.Body>
-            <div className="flex h-[200px] w-full items-center justify-center">Content</div>
-          </Modal.Body>
-          <Modal.Footer>
-            <div className={footerClasses}>
-              <div className={footerLeftBoxClasses}>
-                <Checkbox
-                  id="modal-checkbox"
-                  label="ラベル"
-                  isChecked={isChecked}
-                  onChange={() => setIsChecked((prev) => !prev)}
-                />
-              </div>
-              <div className={footerButtonClasses}>
-                <Button
-                  variant="outline"
-                  size="large"
-                  onClick={action('キャンセル')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
-                  キャンセル
-                </Button>
-                <Button
-                  variant="fill"
-                  size="large"
-                  onClick={action('保存する')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
-                  保存する
-                </Button>
-              </div>
-            </div>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  },
-};
-
-export const SizeSmallWithSubButton: Story = {
-  args: {
-    width: widthSmall,
-  },
-  render: function MyFunc({ ...args }) {
-    const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'justify-between', {
-      'flex-col-reverse gap-4': args.width < widthLimit,
-    });
-    const footerLeftBoxClasses = 'flex items-center flex-wrap';
-    const footerButtonClasses = clsx('flex', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
-    return (
-      <div>
-        <button type="button" onClick={() => setIsOpen(true)}>
-          open
-        </button>
-        <Modal isOpen setIsOpen={setIsOpen} width={args.width}>
-          <Modal.Header>タイトル</Modal.Header>
-          <Modal.Body>
-            <div className="flex h-[200px] w-full items-center justify-center">Content</div>
-          </Modal.Body>
-          <Modal.Footer>
-            <div className={footerClasses}>
-              <div className={footerLeftBoxClasses}>
-                <Button variant="text" size="large" onClick={action('ボタンラベル')}>
-                  ボタンラベル
-                </Button>
-              </div>
-              <div className={footerButtonClasses}>
-                <Button
-                  variant="outline"
-                  size="large"
-                  onClick={action('キャンセル')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
-                  キャンセル
-                </Button>
-                <Button
-                  variant="fill"
-                  size="large"
-                  onClick={action('保存する')}
-                  width={args.width < widthLimit ? 132 : 'auto'}
-                >
+                <Button variant="fill" size="large" onClick={action('保存する')}>
                   保存する
                 </Button>
               </div>
@@ -373,16 +136,11 @@ export const SizeSmallWithSubButton: Story = {
 
 export const FixedHeight: Story = {
   args: {
-    width: widthMedium,
+    width: 480,
   },
   render: function MyFunc({ ...args }) {
     const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
+
     return (
       <div>
         <button type="button" onClick={() => setIsOpen(true)}>
@@ -394,21 +152,11 @@ export const FixedHeight: Story = {
             <div className="flex h-[800px] w-full items-center justify-center">Content</div>
           </Modal.Body>
           <Modal.Footer>
-            <div className={footerClasses}>
-              <Button
-                variant="outline"
-                size="large"
-                onClick={action('キャンセル')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+            <div className="flex w-full flex-wrap items-center justify-end gap-4">
+              <Button variant="outline" size="large" onClick={action('キャンセル')}>
                 キャンセル
               </Button>
-              <Button
-                variant="fill"
-                size="large"
-                onClick={action('保存する')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+              <Button variant="fill" size="large" onClick={action('保存する')}>
                 保存する
               </Button>
             </div>
@@ -421,22 +169,17 @@ export const FixedHeight: Story = {
 
 export const WithTabs: Story = {
   args: {
-    width: widthMedium,
+    width: 480,
   },
   render: function MyFunc({ ...args }) {
     const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
     const [selectedTab, setSelectedTab] = useState('tab1');
     const tabItems = [
       { id: 'tab1', label: 'タブラベル1' },
       { id: 'tab2', label: 'タブラベル2' },
       { id: 'tab3', label: 'タブラベル3' },
     ];
+
     return (
       <div>
         <button type="button" onClick={() => setIsOpen(true)}>
@@ -463,21 +206,11 @@ export const WithTabs: Story = {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <div className={footerClasses}>
-              <Button
-                variant="outline"
-                size="large"
-                onClick={action('キャンセル')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+            <div className="flex w-full flex-wrap items-center justify-end gap-4">
+              <Button variant="outline" size="large" onClick={action('キャンセル')}>
                 キャンセル
               </Button>
-              <Button
-                variant="fill"
-                size="large"
-                onClick={action('保存する')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+              <Button variant="fill" size="large" onClick={action('保存する')}>
                 保存する
               </Button>
             </div>
@@ -490,7 +223,7 @@ export const WithTabs: Story = {
 
 export const WithoutButton: Story = {
   args: {
-    width: widthMedium,
+    width: 480,
   },
   render: function MyFunc({ ...args }) {
     const [_, setIsOpen] = useState(false);
@@ -516,12 +249,7 @@ export const Danger: Story = {
   },
   render: function MyFunc({ ...args }) {
     const [_, setIsOpen] = useState(false);
-    const footerClasses = clsx('flex', 'w-full', 'items-center', 'flex-wrap', {
-      'justify-end': args.width >= widthLimit,
-      'justify-center': args.width < widthLimit,
-      'gap-2': args.width < widthLimit,
-      'gap-4': widthLimit <= args.width,
-    });
+
     return (
       <div>
         <button type="button" onClick={() => setIsOpen(true)}>
@@ -535,21 +263,11 @@ export const Danger: Story = {
             <div className="flex h-16 w-full items-center justify-center">Content</div>
           </Modal.Body>
           <Modal.Footer isNoBorder>
-            <div className={footerClasses}>
-              <Button
-                variant="outline"
-                size="large"
-                onClick={action('キャンセル')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+            <div className="flex w-full flex-wrap items-center justify-end gap-4">
+              <Button variant="outline" size="large" onClick={action('キャンセル')}>
                 キャンセル
               </Button>
-              <Button
-                variant="fillDanger"
-                size="large"
-                onClick={action('削除する')}
-                width={args.width < widthLimit ? 132 : 'auto'}
-              >
+              <Button variant="fillDanger" size="large" onClick={action('削除する')}>
                 削除する
               </Button>
             </div>
