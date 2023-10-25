fec394813722c29533707ee5bdcae0ee64398cf4
yutakam80 <yutakam80@gmail.com>
2023-10-25 12:30:47 +0900

    fix: body style

--- packages/component-ui/src/modal/modal-body.tsx
+++ packages/component-ui/src/modal/modal-body.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
 
 export function ModalBody({ children }: PropsWithChildren) {
-  return <div className="flex items-center justify-center overflow-y-auto">{children}</div>;
+  return <div className="overflow-y-auto">{children}</div>;
 }
