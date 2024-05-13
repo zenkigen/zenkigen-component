f3b051009a148ea03c983c67e77f79402fb3e9b8
TatsuroIde <Orust4t@gmail.com>
2024-05-13 09:52:36 +0900

    feat: add story for linebreak

--- packages/component-ui/src/tooltip/tooltip.stories.tsx
+++ packages/component-ui/src/tooltip/tooltip.stories.tsx
@@ -64,3 +64,28 @@ export const Portal: Story = {
     </div>
   ),
 };
+
+export const WithLineBreak: Story = {
+  args: {
+    content: (
+      <>
+        行1<br />
+        行2
+      </>
+    )
+  },
+  render: (args) => (
+    <div className="grid gap-10 px-20 py-10">
+      <div className="flex items-center gap-20">
+        <Tooltip {...args}>
+          <div className="flex h-10 w-24 items-center justify-center rounded border border-gray-400">target</div>
+        </Tooltip>
+      </div>
+      <div className="flex items-center gap-20">
+        <Tooltip {...args}>
+          <div className="flex h-10 w-[240px] items-center justify-center rounded border border-gray-400">target</div>
+        </Tooltip>
+      </div>
+    </div>
+  ),
+};
