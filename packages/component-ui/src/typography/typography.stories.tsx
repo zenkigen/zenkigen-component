1bed75ccef6cb2fdbb4ac9ce35e4cd831597639f
kenichi iida <kenichi.iida@quq.jp>
2024-06-25 15:20:07 +0900

    微調整

--- packages/component-ui/src/typography/typography.stories.tsx
+++ packages/component-ui/src/typography/typography.stories.tsx
@@ -2,12 +2,12 @@ export default {
   component: null,
 };
 
-type Props = {
+type SampleTextProps = {
   label: string;
   className: string;
 };
 
-function Sampletext({ label, className }: Props) {
+function SampleText({ label, className }: SampleTextProps) {
   return (
     <div className="flex-1">
       <div style={{ fontSize: 14 }}>{label}</div>
@@ -24,22 +24,20 @@ export function Body() {
   const classes = 'flex gap-4';
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="body3regular" className="typography-body3regular" />
-          <Sampletext label="body3bold" className="typography-body3bold" />
-        </div>
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="body2regular" className="typography-body2regular" />
-          <Sampletext label="body2bold" className="typography-body2bold" />
-        </div>
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="body1regular" className="typography-body1regular" />
-          <Sampletext label="body1bold" className="typography-body1bold" />
-        </div>
+    <div className="flex flex-col">
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="body3regular" className="typography-body3regular" />
+        <SampleText label="body3bold" className="typography-body3bold" />
       </div>
-    </>
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="body2regular" className="typography-body2regular" />
+        <SampleText label="body2bold" className="typography-body2bold" />
+      </div>
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="body1regular" className="typography-body1regular" />
+        <SampleText label="body1bold" className="typography-body1bold" />
+      </div>
+    </div>
   );
 }
 
@@ -47,25 +45,23 @@ export function Label() {
   const classes = 'flex gap-4';
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="label4regular" className="typography-label4regular" />
-          <Sampletext label="label4bold" className="typography-label4bold" />
-        </div>
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="label3regular" className="typography-label3regular" />
-          <Sampletext label="label3bold" className="typography-label3bold" />
-        </div>
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="label2regular" className="typography-label2regular" />
-          <Sampletext label="label2bold" className="typography-label2bold" />
-        </div>
-        <div className={classes} style={{ width: WIDTH }}>
-          <Sampletext label="label1regular" className="typography-label1regular" />
-          <Sampletext label="label1bold" className="typography-label1bold" />
-        </div>
+    <div className="flex flex-col">
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="label4regular" className="typography-label4regular" />
+        <SampleText label="label4bold" className="typography-label4bold" />
       </div>
-    </>
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="label3regular" className="typography-label3regular" />
+        <SampleText label="label3bold" className="typography-label3bold" />
+      </div>
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="label2regular" className="typography-label2regular" />
+        <SampleText label="label2bold" className="typography-label2bold" />
+      </div>
+      <div className={classes} style={{ width: WIDTH }}>
+        <SampleText label="label1regular" className="typography-label1regular" />
+        <SampleText label="label1bold" className="typography-label1bold" />
+      </div>
+    </div>
   );
 }
