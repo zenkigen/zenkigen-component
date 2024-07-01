30de3d723a6ae85390b15482e3cbd026baa619b0
TatsuroIde <Orust4t@gmail.com>
2024-07-01 16:18:54 +0900

    feat: sticky header for table

--- packages/component-ui/src/table/table-cell.tsx
+++ packages/component-ui/src/table/table-cell.tsx
@@ -4,8 +4,13 @@ import type { ReactNode } from 'react';
 type Props = {
   children?: ReactNode;
   className?: string;
+  isHeader?: boolean;
 };
 
-export function TableCell({ children, className }: Props) {
-  return <div className={clsx('border-b border-uiBorder01', className)}>{children}</div>;
+export function TableCell({ children, className, isHeader }: Props) {
+  return (
+    <div className={clsx('border-b border-uiBorder01', isHeader && 'sticky top-0 bg-white z-10', className)}>
+      {children}
+    </div>
+  );
 }
