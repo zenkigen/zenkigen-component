6e0caa92eb237fc399fcf223bd7085f52c1e2763
TatsuroIde <Orust4t@gmail.com>
2024-07-03 19:04:44 +0900

    refactor: isheader condition

--- packages/component-ui/src/table/table-cell.tsx
+++ packages/component-ui/src/table/table-cell.tsx
@@ -9,9 +9,7 @@ type Props = {
 
 export function TableCell({ children, className, isHeader }: Props) {
   return (
-    <div
-      className={clsx('border-b border-uiBorder01', isHeader === true ? 'sticky top-0 z-10 bg-white' : '', className)}
-    >
+    <div className={clsx('border-b border-uiBorder01', isHeader && 'sticky top-0 z-10 bg-white', className)}>
       {children}
     </div>
   );
