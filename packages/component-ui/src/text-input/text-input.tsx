920e9765cc85c34bdaac188cb6e179e9f38640c6
kenichi iida <kenichi.iida@quq.jp>
2025-03-13 12:15:30 +0900

    fix lint

--- packages/component-ui/src/text-input/text-input.tsx
+++ packages/component-ui/src/text-input/text-input.tsx
@@ -13,7 +13,7 @@ type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> & {
 
 export const TextInput = forwardRef<HTMLInputElement, Props>(
   ({ size = 'medium', isError = false, disabled = false, onClickClearButton, ...props }: Props, ref) => {
-    const showClearButton = onClickClearButton && props.value.length !== 0 && !disabled;
+    const isShowClearButton = onClickClearButton && props.value.length !== 0 && !disabled ? true : false;
     const inputWrapClasses = clsx('relative flex items-center gap-2 overflow-hidden rounded border', {
       'border-uiBorder02': !isError && !disabled,
       'border-supportError': isError && !disabled,
@@ -21,24 +21,21 @@ export const TextInput = forwardRef<HTMLInputElement, Props>(
       'hover:focus-within:border-activeInput': !isError,
       'focus-within:border-activeInput': !isError,
       'bg-disabled02 border-disabled01': disabled,
-      'pr-2':showClearButton,
+      'pr-2': isShowClearButton,
     });
 
-    const inputClasses = clsx(
-      'flex-1 pl-2 outline-0 placeholder:text-textPlaceholder disabled:text-textPlaceholder',
-      {
-        ['typography-label14regular min-h-8']: size === 'medium',
-        ['typography-label16regular min-h-10']: size === 'large',
-        'text-text01': !isError,
-        'text-supportError': isError,
-        'pr-3':!showClearButton,
-      },
-    );
+    const inputClasses = clsx('flex-1 pl-2 outline-0 placeholder:text-textPlaceholder disabled:text-textPlaceholder', {
+      ['typography-label14regular min-h-8']: size === 'medium',
+      ['typography-label16regular min-h-10']: size === 'large',
+      'text-text01': !isError,
+      'text-supportError': isError,
+      'pr-3': !isShowClearButton,
+    });
 
     return (
       <div className={inputWrapClasses}>
         <input ref={ref} size={1} className={inputClasses} disabled={disabled} onChange={props.onChange} {...props} />
-        {showClearButton && (
+        {isShowClearButton && (
           <IconButton variant="text" icon="close" size="small" isNoPadding onClick={onClickClearButton} />
         )}
       </div>
