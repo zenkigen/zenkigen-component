1c2a355976d6308ef5f3264d92dbadbc9938d071
kenichi iida <kenichi.iida@quq.jp>
2025-03-27 16:01:17 +0900

    fix(tab-item): update hover background color for unselected tabs
    - Changed the hover background color class from 'bg-uiBorder02Dark' to 'bg-uiBorder04' for unselected tab items.

--- packages/component-ui/src/tab/tab-item.tsx
+++ packages/component-ui/src/tab/tab-item.tsx
@@ -14,7 +14,7 @@ export const TabItem = ({ isSelected = false, ...props }: Props) => {
     'relative z-0 flex py-2 leading-[24px] before:absolute before:inset-x-0 before:bottom-0 before:h-px hover:text-text01 disabled:pointer-events-none disabled:text-disabled01',
     {
       'typography-label14regular': !isSelected,
-      'text-interactive02 hover:before:bg-uiBorder02Dark': !isSelected,
+      'text-interactive02 hover:before:bg-uiBorder04': !isSelected,
       'typography-label14bold': isSelected,
       'before:bg-interactive01 hover:before:bg-interactive01 pointer-events-none': isSelected,
     },
