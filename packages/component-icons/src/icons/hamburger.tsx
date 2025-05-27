544eafc481b6e8e17644b8a30a6af913d1341606
kenichi iida <kenichi.iida@quq.jp>
2025-05-27 16:09:10 +0900

    feat(icons): add multiple new icon components and handle errors in codegen execution

---
+++ packages/component-icons/src/icons/hamburger.tsx
@@ -0,0 +1,14 @@
+/*
+* NOTE: This file is auto generated
+* Do not edit manually.
+*/
+import React from 'react';
+
+export const hamburgerIcon: React.ReactElement = (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img">
+<title>hamburger</title>
+<path d="M21.75 18.75H2.25C1.84 18.75 1.5 18.41 1.5 18C1.5 17.59 1.84 17.25 2.25 17.25H21.75C22.16 17.25 22.5 17.59 22.5 18C22.5 18.41 22.16 18.75 21.75 18.75Z"></path>
+<path d="M21.75 12.75H2.25C1.84 12.75 1.5 12.41 1.5 12C1.5 11.59 1.84 11.25 2.25 11.25H21.75C22.16 11.25 22.5 11.59 22.5 12C22.5 12.41 22.16 12.75 21.75 12.75Z"></path>
+<path d="M21.75 6.75H2.25C1.84 6.75 1.5 6.41 1.5 6C1.5 5.59 1.84 5.25 2.25 5.25H21.75C22.16 5.25 22.5 5.59 22.5 6C22.5 6.41 22.16 6.75 21.75 6.75Z"></path>
+</svg>
+);
