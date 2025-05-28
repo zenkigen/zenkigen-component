cb79710dc4bbd23be7950f4c161a33a78a97b27c
kenichi iida <kenichi.iida@quq.jp>
2025-05-28 16:08:46 +0900

    Add fourth layer to breadcrumb story

--- packages/component-ui/src/breadcrumb/breadcrumb.stories.tsx
+++ packages/component-ui/src/breadcrumb/breadcrumb.stories.tsx
@@ -13,6 +13,7 @@ const breadcrumbList = [
   { key: 1, label: 'ホーム', href: '/' },
   { key: 2, label: '2層目', href: '/about' },
   { key: 3, label: '3層目' },
+  { key: 4, label: '4層目' },
 ];
 
 export function Base() {
