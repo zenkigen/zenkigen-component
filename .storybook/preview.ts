0b0e51ff6fb5aa3fc9836889e1b07fb3aef308cd
kenichi iida <kenichi.iida@quq.jp>
2025-06-02 10:22:09 +0900

    fix(storybook): update React import in preview.ts and adjust import order in main.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 import './globals.css';
 
 import type { Preview } from '@storybook/react';
+import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any).React = React;
 
 const preview: Preview = {
   parameters: {
