2d19cd3eb8a337bf20e2e6ab83df8f1f9f8ec836
kenichi iida <kenichi.iida@quq.jp>
2025-06-05 14:01:29 +0900

    Refactor icon components for improved consistency and performance across the component-icons package

--- packages/component-icons/src/icons/sort-up.tsx
+++ packages/component-icons/src/icons/sort-up.tsx
@@ -5,7 +5,7 @@
 import type React from 'react';
 
 export const sortupIcon: React.ReactElement = (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img"><title>sortUp</title>
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="sortUp">
 <path fillRule="evenodd" clipRule="evenodd" d="M6 1.5C6.27614 1.5 6.5 1.72386 6.5 2V22C6.5 22.2761 6.27614 22.5 6 22.5C5.72386 22.5 5.5 22.2761 5.5 22V2C5.5 1.72386 5.72386 1.5 6 1.5Z"></path>
 <path fillRule="evenodd" clipRule="evenodd" d="M5.64645 1.64645C5.84171 1.45118 6.15829 1.45118 6.35355 1.64645L10.3536 5.64645C10.5488 5.84171 10.5488 6.15829 10.3536 6.35355C10.1583 6.54882 9.84171 6.54882 9.64645 6.35355L6 2.70711L2.35355 6.35355C2.15829 6.54882 1.84171 6.54882 1.64645 6.35355C1.45118 6.15829 1.45118 5.84171 1.64645 5.64645L5.64645 1.64645Z"></path>
 <path fillRule="evenodd" clipRule="evenodd" d="M9.5 22C9.5 21.7239 9.72386 21.5 10 21.5H14C14.2761 21.5 14.5 21.7239 14.5 22C14.5 22.2761 14.2761 22.5 14 22.5H10C9.72386 22.5 9.5 22.2761 9.5 22Z"></path>
