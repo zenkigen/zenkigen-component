020605c86046ecca1cc730f2d2bc9ba3cfee29ea
kenichi iida <kenichi.iida@quq.jp>
2025-09-09 10:58:45 +0900

    feat(Tab): enhance documentation and examples for Tab component with layout options

--- packages/component-ui/src/tab/tab.stories.tsx
+++ packages/component-ui/src/tab/tab.stories.tsx
@@ -1,144 +1,281 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
+import { Button } from '../button';
 import { Tab } from '.';
 
-const meta: Meta<typeof Tab.Item> = {
-  title: 'Components/Tabs',
-  component: Tab.Item,
-};
+const meta = {
+  title: 'Components/Tab',
+  component: Tab,
+  parameters: {
+    docs: {
+      source: {
+        code: ``,
+      },
+    },
+  },
+  argTypes: {
+    layout: { control: 'radio', options: ['auto', 'equal'], description: 'レイアウト' },
+  },
+} satisfies Meta<typeof Tab>;
 
 export default meta;
-type Story = StoryObj<typeof Tab.Item>;
+type Story = StoryObj<typeof meta>;
+
+const TabExample = ({
+  layout = 'auto',
+  children,
+  onChange,
+}: {
+  layout?: 'auto' | 'equal';
+  children: React.ReactNode;
+  onChange?: (value: string) => void;
+}) => {
+  const [selectedTab, setSelectedTab] = useState('tab1');
+
+  return (
+    <Tab layout={layout}>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child) && child.type === Tab.Item) {
+          type TabItemProps = {
+            id: string;
+            isDisabled?: boolean;
+            onClick?: (id: string) => void;
+            isSelected?: boolean;
+          };
+
+          const props = child.props as TabItemProps;
+
+          return React.cloneElement(child as React.ReactElement<TabItemProps>, {
+            // eslint-disable-next-line react/prop-types
+            isSelected: selectedTab === props.id,
+            onClick: (id: string) => {
+              setSelectedTab(id);
+              onChange?.(id);
+            },
+          });
+        }
+
+        return child;
+      })}
+    </Tab>
+  );
+};
 
 export const Component: Story = {
+  args: {
+    layout: 'auto',
+    children: <></>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
   parameters: {
     chromatic: { disable: true },
   },
-  render: function MyFunc() {
+  render: function MyFunc(args) {
     const [selectedTab, setSelectedTab] = useState('tab1');
+    const [disabledTab, setDisabledTab] = useState('tab3');
 
     return (
-      <Tab>
-        <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab}>
-          タブラベル
-        </Tab.Item>
-        <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab}>
-          タブラベル
-        </Tab.Item>
-        <Tab.Item id="tab3" isSelected={selectedTab === 'tab3'} onClick={setSelectedTab} isDisabled>
-          タブラベル
-        </Tab.Item>
-      </Tab>
+      <div className="flex flex-col gap-5">
+        <Tab {...args}>
+          <Tab.Item
+            id="tab1"
+            isSelected={selectedTab === 'tab1'}
+            onClick={setSelectedTab}
+            isDisabled={disabledTab === 'tab1'}
+          >
+            短い
+          </Tab.Item>
+          <Tab.Item
+            id="tab2"
+            isSelected={selectedTab === 'tab2'}
+            onClick={setSelectedTab}
+            isDisabled={disabledTab === 'tab2'}
+          >
+            中程度のタブ
+          </Tab.Item>
+          <Tab.Item
+            id="tab3"
+            isSelected={selectedTab === 'tab3'}
+            onClick={setSelectedTab}
+            isDisabled={disabledTab === 'tab3'}
+          >
+            とても長いタブのテキスト
+          </Tab.Item>
+        </Tab>
+        <div className="flex flex-col gap-5">
+          <div className="typography-label12regular">
+            {selectedTab === 'tab1' && '「短い」が選択されています'}
+            {selectedTab === 'tab2' && '「中程度のタブ」が選択されています'}
+            {selectedTab === 'tab3' && '「とても長いタブのテキスト」が選択されています'}
+          </div>
+          <div className="flex flex-wrap gap-2">
+            <Button
+              onClick={() => {
+                setDisabledTab('tab1');
+              }}
+            >
+              「短い」を無効にする
+            </Button>
+            <Button
+              onClick={() => {
+                setDisabledTab('tab2');
+              }}
+            >
+              「中程度のタブ」を無効にする
+            </Button>
+            <Button
+              onClick={() => {
+                setDisabledTab('tab3');
+              }}
+            >
+              「とても長いタブのテキスト」を無効にする
+            </Button>
+            <Button
+              onClick={() => {
+                setDisabledTab('');
+              }}
+            >
+              無効を解除
+            </Button>
+          </div>
+        </div>
+      </div>
     );
   },
 };
 
-export const ComponentItem: Story = {
+export const LayoutAutoExample: Story = {
   args: {
-    children: 'タブラベル',
-    id: 'tab1',
-    isSelected: true,
-    isDisabled: false,
+    layout: 'auto',
+    children: <></>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  render: (args) => (
+    <div className="flex items-center justify-center">
+      <TabExample {...args}>
+        <Tab.Item id="tab1" onClick={() => {}}>
+          短い
+        </Tab.Item>
+        <Tab.Item id="tab2" onClick={() => {}}>
+          中程度のタブ
+        </Tab.Item>
+        <Tab.Item id="tab3" onClick={() => {}}>
+          とても長いタブのテキスト
+        </Tab.Item>
+      </TabExample>
+    </div>
+  ),
+};
+
+export const LayoutEqualExample: Story = {
+  args: {
+    layout: 'equal',
+    children: <></>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  render: (args) => (
+    <div className="flex items-center justify-center">
+      <TabExample {...args}>
+        <Tab.Item id="tab1" onClick={() => {}}>
+          短い
+        </Tab.Item>
+        <Tab.Item id="tab2" onClick={() => {}}>
+          中程度のタブ
+        </Tab.Item>
+        <Tab.Item id="tab3" onClick={() => {}}>
+          とても長いタブのテキスト
+        </Tab.Item>
+      </TabExample>
+    </div>
+  ),
+};
+
+export const LayoutExamples: Story = {
+  args: {
+    layout: 'auto',
+    children: <></>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
   },
   parameters: {
     chromatic: { disable: true },
   },
-  render: (args) => (
-    <Tab>
-      <Tab.Item {...args} />
-    </Tab>
-  ),
-};
+  render: () => (
+    <div>
+      <div className="flex w-full flex-col items-center gap-5 p-5">
+        <TabExample layout="auto">
+          <Tab.Item id="tab1" onClick={() => {}}>
+            短い
+          </Tab.Item>
+          <Tab.Item id="tab2" onClick={() => {}}>
+            中程度のタブ
+          </Tab.Item>
+          <Tab.Item id="tab3" onClick={() => {}}>
+            とても長いタブのテキスト
+          </Tab.Item>
+        </TabExample>
 
-export function Base() {
-  const [selectedTab, setSelectedTab] = useState('tab1');
+        <TabExample layout="equal">
+          <Tab.Item id="tab4" onClick={() => {}}>
+            短い
+          </Tab.Item>
+          <Tab.Item id="tab5" onClick={() => {}}>
+            中程度のタブ
+          </Tab.Item>
+          <Tab.Item id="tab6" onClick={() => {}}>
+            とても長いタブのテキスト
+          </Tab.Item>
+        </TabExample>
+      </div>
+      <div className="flex w-full flex-col gap-5 p-5">
+        <TabExample layout="auto">
+          <Tab.Item id="tab1" onClick={() => {}}>
+            短い
+          </Tab.Item>
+          <Tab.Item id="tab2" onClick={() => {}}>
+            中程度のタブ
+          </Tab.Item>
+          <Tab.Item id="tab3" onClick={() => {}}>
+            とても長いタブのテキスト
+          </Tab.Item>
+        </TabExample>
 
-  return (
-    <Tab>
-      <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab}>
-        タブラベル
-      </Tab.Item>
-      <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab}>
-        タブラベル
-      </Tab.Item>
-      <Tab.Item id="tab3" isDisabled onClick={setSelectedTab}>
-        タブラベル
-      </Tab.Item>
-    </Tab>
-  );
-}
-
-export function LayoutExamples() {
-  const [selectedTab, setSelectedTab] = useState('tab1');
-
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="">
-        <Tab>
-          <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab}>
-            発言別
+        <TabExample layout="equal">
+          <Tab.Item id="tab4" onClick={() => {}}>
+            短い
           </Tab.Item>
-          <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab}>
-            カテゴリ別
+          <Tab.Item id="tab5" onClick={() => {}}>
+            中程度のタブ
           </Tab.Item>
-          <Tab.Item id="tab3" isSelected={selectedTab === 'tab3'} onClick={setSelectedTab}>
-            面談担当者別
+          <Tab.Item id="tab6" onClick={() => {}}>
+            とても長いタブのテキスト
           </Tab.Item>
-        </Tab>
-      </div>
-      <div className="flex flex-col items-start gap-4">
-        <Tab>
-          <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab}>
-            発言別
-          </Tab.Item>
-          <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab}>
-            カテゴリ別
-          </Tab.Item>
-          <Tab.Item id="tab3" isSelected={selectedTab === 'tab3'} onClick={setSelectedTab}>
-            面談担当者別
-          </Tab.Item>
-        </Tab>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <Tab>
-          <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab}>
-            発言別
-          </Tab.Item>
-          <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab}>
-            カテゴリ別
-          </Tab.Item>
-          <Tab.Item id="tab3" isSelected={selectedTab === 'tab3'} onClick={setSelectedTab}>
-            面談担当者別
-          </Tab.Item>
-        </Tab>
-      </div>
-      <div className="flex flex-col items-end gap-4">
-        <Tab>
-          <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab}>
-            発言別
-          </Tab.Item>
-          <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab}>
-            カテゴリ別
-          </Tab.Item>
-          <Tab.Item id="tab3" isSelected={selectedTab === 'tab3'} onClick={setSelectedTab}>
-            面談担当者別
-          </Tab.Item>
-        </Tab>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <Tab>
-          <Tab.Item id="tab1" isSelected={selectedTab === 'tab1'} onClick={setSelectedTab} minWidth="160px">
-            発言別
-          </Tab.Item>
-          <Tab.Item id="tab2" isSelected={selectedTab === 'tab2'} onClick={setSelectedTab} minWidth="160px">
-            カテゴリ別
-          </Tab.Item>
-          <Tab.Item id="tab3" isSelected={selectedTab === 'tab3'} onClick={setSelectedTab} minWidth="160px">
-            面談担当者別
-          </Tab.Item>
-        </Tab>
+        </TabExample>
       </div>
     </div>
-  );
-}
+  ),
+};
