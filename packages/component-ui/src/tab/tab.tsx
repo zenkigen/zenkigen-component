10c89c59614116b5baed3a492a50d80cd03bd9cb
kenichi iida <kenichi.iida@quq.jp>
2025-09-09 10:58:32 +0900

    feat(Tab): add layout prop for flexible tab arrangement

--- packages/component-ui/src/tab/tab.tsx
+++ packages/component-ui/src/tab/tab.tsx
@@ -1,17 +1,28 @@
+import { clsx } from 'clsx';
 import type { ReactNode } from 'react';
+import { Children } from 'react';
 
 import { TabItem } from './tab-item';
 
 type Props = {
   children?: ReactNode;
+  layout?: 'auto' | 'equal';
 };
 
-export function Tab({ children }: Props) {
+export function Tab({ children, layout = 'auto' }: Props) {
+  const childrenCount = Children.count(children);
+  const containerStyle = layout === 'equal' ? { gridTemplateColumns: `repeat(${childrenCount}, minmax(0,1fr))` } : {};
+
+  const containerClasses = clsx(
+    'relative px-6 before:absolute before:inset-x-0 before:bottom-0 before:h-px before:bg-uiBorder01',
+    {
+      'flex gap-4': layout === 'auto',
+      'grid gap-4': layout === 'equal',
+    },
+  );
+
   return (
-    <div
-      role="tablist"
-      className="relative flex gap-4 px-6 before:absolute before:inset-x-0 before:bottom-0 before:h-px before:bg-uiBorder01"
-    >
+    <div role="tablist" className={containerClasses} style={containerStyle}>
       {children}
     </div>
   );
