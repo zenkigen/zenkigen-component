946d389ef1a5df88583984e6d33ffa2ab1b44112
kenichi iida <kenichi.iida@quq.jp>
2025-10-03 16:49:38 +0900

    feat(select): integrate Floating UI for improved dropdown positioning and add new props for floating styles and reference

--- packages/component-ui/src/select/select-context.ts
+++ packages/component-ui/src/select/select-context.ts
@@ -1,3 +1,4 @@
+import type { CSSProperties } from 'react';
 import { createContext } from 'react';
 
 import type { SelectOption } from './type';
@@ -10,6 +11,8 @@ type UseSelectReturnType = {
   onChange?: (option: SelectOption | null) => void;
   variant?: 'text' | 'outline';
   isError?: boolean;
+  floatingStyles?: CSSProperties;
+  floatingRef?: React.MutableRefObject<HTMLElement | null>;
 };
 
 export const SelectContext = createContext<UseSelectReturnType>({
