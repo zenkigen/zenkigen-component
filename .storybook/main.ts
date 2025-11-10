38ab72c4bed303e273846f8fed4462d91d33fd4c
kenichi iida <kenichi.iida@quq.jp>
2025-11-10 16:24:29 +0900

    fix lint

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import { createRequire } from "node:module";
+import { createRequire } from 'node:module';
 import type { StorybookConfig } from '@storybook/react-vite';
 import path, { dirname, join } from 'path';
 
@@ -6,9 +6,9 @@ const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
   stories: ['../packages/**/*.stories.@(ts|tsx)', '../packages/**/*.mdx'],
-  addons: [getAbsolutePath("@storybook/addon-a11y"), getAbsolutePath("@storybook/addon-docs")],
+  addons: [getAbsolutePath('@storybook/addon-a11y'), getAbsolutePath('@storybook/addon-docs')],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
   viteFinal: async (config) => {
@@ -38,5 +38,5 @@ const config: StorybookConfig = {
 export default config;
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
