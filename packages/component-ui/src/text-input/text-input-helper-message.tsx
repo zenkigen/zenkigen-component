717461746e57ecdbef114e6560e0d593208b079b
kenichi iida <kenichi.iida@quq.jp>
2025-11-21 14:49:42 +0900

    fix lint

--- packages/component-ui/src/text-input/text-input-helper-message.tsx
+++ packages/component-ui/src/text-input/text-input-helper-message.tsx
@@ -6,14 +6,12 @@ import { useTextInputCompoundContext } from './text-input-context';
 
 export type TextInputHelperMessageProps = Omit<HTMLAttributes<HTMLDivElement>, 'className'>;
 
-export const TextInputHelperMessage = forwardRef<HTMLDivElement, TextInputHelperMessageProps>(
-  (props, ref) => {
-    const { inputProps } = useTextInputCompoundContext('TextInput.HelperMessage');
-    const typographyClass = inputProps.size === 'large' ? 'typography-label12regular' : 'typography-label11regular';
-    const helperMessageClassName = clsx(typographyClass, 'text-text02');
+export const TextInputHelperMessage = forwardRef<HTMLDivElement, TextInputHelperMessageProps>((props, ref) => {
+  const { inputProps } = useTextInputCompoundContext('TextInput.HelperMessage');
+  const typographyClass = inputProps.size === 'large' ? 'typography-label12regular' : 'typography-label11regular';
+  const helperMessageClassName = clsx(typographyClass, 'text-text02');
 
-    return <div ref={ref} className={helperMessageClassName} {...props} />;
-  },
-);
+  return <div ref={ref} className={helperMessageClassName} {...props} />;
+});
 
 TextInputHelperMessage.displayName = 'TextInput.HelperMessage';
