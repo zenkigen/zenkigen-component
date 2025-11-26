f0209777af6ae8a3b44ee48bd23a37d89a045dde
kenichi iida <kenichi.iida@quq.jp>
2025-11-26 16:16:53 +0900

    fix(text-area): classNameの変数名を修正し、型定義を更新

--- packages/component-ui/src/text-area/text-area.types.ts
+++ packages/component-ui/src/text-area/text-area.types.ts
@@ -1,6 +1,10 @@
 import type { CSSProperties, TextareaHTMLAttributes } from 'react';
 
 export type TextAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  /**
+   * @deprecated 外部から className を渡してスタイルを上書きすることは非推奨です。
+   */
+  className?: string;
   size?: 'medium' | 'large';
   value: string;
   height?: CSSProperties['height'];
