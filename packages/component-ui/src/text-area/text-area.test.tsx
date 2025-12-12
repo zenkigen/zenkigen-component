7ac1c30f5187f726fee2cd3bdd72270d83ae1dec
kenichi iida <kenichi.iida@quq.jp>
2025-12-12 16:02:04 +0900

    update test

--- packages/component-ui/src/text-area/text-area.test.tsx
+++ packages/component-ui/src/text-area/text-area.test.tsx
@@ -51,8 +51,7 @@ describe('TextArea', () => {
       const textarea = screen.getByTestId('textarea');
       expect(textarea.className).toMatch(/typography-body14regular/);
       expect(textarea.className).toMatch(/px-2/);
-      expect(textarea.className).toMatch(/pt-2/);
-      expect(textarea.className).toMatch(/pb-2/);
+      expect(textarea.className).toMatch(/py-1.5/);
     });
 
     it('largeサイズのスタイルが適用されること', () => {
@@ -61,7 +60,7 @@ describe('TextArea', () => {
       expect(textarea.className).toMatch(/text-4/);
       expect(textarea.className).toMatch(/leading-normal/);
       expect(textarea.className).toMatch(/px-3\.5/);
-      expect(textarea.className).toMatch(/py-2\.5/);
+      expect(textarea.className).toMatch(/py-2/);
     });
   });
 
