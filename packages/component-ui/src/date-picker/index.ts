823e9f3b65543b43cb135dc2373ccbf9bed98b05
kenichi iida <kenichi.iida@quq.jp>
2026-01-16 15:14:24 +0900

    fix lint

--- packages/component-ui/src/date-picker/index.ts
+++ packages/component-ui/src/date-picker/index.ts
@@ -1,3 +1,3 @@
-export { DatePicker } from './date-picker';
 export type { DatePickerProps } from './date-picker';
+export { DatePicker } from './date-picker';
 export type { DatePickerTimeZone } from './date-picker-utils';
